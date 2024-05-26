8b67102ae2e01ec85e1b71589b70c903ac60a7a9
benib0 <benjung1067@gmail.com>
2024-05-26 11:04:56 +0200

    Fix sidebar

--- BeniBot/src/routes/+page.svelte
+++ BeniBot/src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
 	import Home from "../components/home.svelte";
+	import Joystick from "../components/joystick.svelte";
 	import { AppBar } from '@skeletonlabs/skeleton';
 	import { AppShell } from '@skeletonlabs/skeleton';
-	import Joystick from "../components/joystick.svelte";
 </script>
 <Home/>
 <Joystick/>
